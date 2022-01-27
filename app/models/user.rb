b72a81330d494f69c4f5538dbd860e6ec8dff682
YukinoSasada <kii.yuki.120@gmail.com>
2022-01-27 11:47:08 +0000

    user update and authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-    before_save { email.downcase! }
-validates :name, presence: true, length: { maximum: 50 }
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true, length: { maximum: 255 },
-                  format: { with: VALID_EMAIL_REGEX },
-                  uniqueness: { case_sensitive: false }
+    before_save { email.downcase! } 
+    # name は必ず存在し、長さが50字以内であること
+    validates :name, presence: true, length: { maximum: 50 }
+    # メールアドレスのフォーマットを「正規表現」を使って指定
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # email は必ず存在し、長さが５０字以内
+    # メールアドレスのフォーマットになっている
+    # メールアドレスはユニーク（同じデータが存在してはいけない）
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: {minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
